eae64e0171c45e1854503ce57eed6a9f46820c34
Alec Shern <alecshern@gmail.com>
2020-05-10 15:24:04 -0500

    Add partitioning of text stream

--- src/clojure_project/core.clj
+++ src/clojure_project/core.clj
@@ -1,11 +1,5 @@
 (ns clojure-project.core)
 
-;(defn readFile []
-;(seq (char-array(slurp "C:\\Users\\alecs\\clojure-project\\resources\\WarAndPeace.txt"))))
-
-;(defn readFile []
-;  (char-array (slurp "C:\\Users\\alecs\\clojure-project\\resources\\WarAndPeace.txt")))
-
 (defn readFile []
    (clojure.string/replace (clojure.string/lower-case (slurp ".\\resources\\WarAndPeace.txt")) #"[^a-z ]" ""))
 
@@ -15,32 +9,34 @@
   (* num_of_chars (- prob_of_chars) (log prob_of_chars)))
 
 
-
-;(defn findTotalInformation [frequency_map probability_map]
-;  (def information_count 0)
-;  (doseq [[key val] (map vector (keys frequency_map) (keys probability_map))]
-;    (def information_count (+ information_count (dataStreamInfo (key) (val)))))
-;  information_count)
-
 (defn findTotalInformation [frequency_map probability_map]
   (def information_count 0)
   (doseq [key (keys frequency_map)]
-    (def information_count (+ information_count (dataStreamInfo (get frequency_map key) (get probability_map key)))))
+    (def information_count (+ information_count (dataStreamInfo (get frequency_map key) (get probability_map key))))) 
   information_count)
+
+(defn findProbabilityMap [frequency_map total_num_chars]
+  (doseq [[k v] (map vector (keys frequency_map) (vals frequency_map))]
+   (def probability_map (assoc probability_map k (double (/ v total_num_chars)))))
+  probability_map)
+
+(defn getNCharFreqs
+  [inputString n]
+  (frequencies (partition n inputString)))
 
 (defn main []
  (def file_as_string (readFile))
  (def frequency_map (frequencies file_as_string))
  (def total_num_chars (count file_as_string))
- (println frequency_map total_num_chars)
+ (println "\nMap with key as letter and value as frequency:" frequency_map total_num_chars)
  ;Make a new map and find probability of each letter to add as key/value pair to probability map
- (def probability_map (sorted-map))
- (doseq [[k v] (map vector (keys frequency_map) (vals frequency_map))]
-   (def probability_map (assoc probability_map k (double (/ v total_num_chars)))))
- ;(doseq [f (keys frequencyMap)]
-   ;(def newMap (assoc newMap f (double (/ (get frequencyMap f) total_num_chars)))))
-  probability_map
+ (def probability_map (findProbabilityMap frequency_map total_num_chars))
+
+(def pairs_frequency_map (getNCharFreqs file_as_string 2))
+(def pairs_probability_map (findProbabilityMap pairs_frequency_map total_num_chars))
   
-  println(findTotalInformation frequency_map probability_map))
+  (println(findTotalInformation frequency_map probability_map))
+  (println(findTotalInformation pairs_frequency_map pairs_probability_map))
+)
 
 (main)
